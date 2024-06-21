88fdb0142bb9f46eca6595e015110054d107d708
Lorro <147809249+Lorr0@users.noreply.github.com>
2024-06-21 09:31:04 +0200

    Update Copy_user_rights.ps1
    Add Copie Group

--- Copy_user_rights.ps1
+++ Copy_user_rights.ps1
@@ -1,1 +1,3 @@
 ﻿Get-ADUser -Identity user1maitre -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members user2client
+
+Get-ADGroupMember "GroupeA" | Get-ADUser | ForEach-Object {Add-ADGroupMember -Identity "GroupeB" -Members $_}
